e2e8a5c88e60a2c7e9159d7c03b47b8f19d7f710
Jose Rogerio de Almeida Junior <almeidajr.vr@gmail.com>
2021-01-18 13:05:40 -0300

    Add ESLint rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
 
-function App() {
+function App(): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img alt="logo" className="App-logo" src={logo} />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
-          target="_blank"
           rel="noopener noreferrer"
+          target="_blank"
         >
           Learn React
         </a>

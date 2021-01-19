9f4eee2b2e0517982554cbd3adcb4134f17b9515
Jose Rogerio de Almeida Junior <almeidajr.vr@gmail.com>
2021-01-19 19:15:07 -0300

    Add more cards in Glassmorphism Card page

--- src/pages/GlassmorphismCard.tsx
+++ src/pages/GlassmorphismCard.tsx
@@ -1,49 +1,29 @@
+import { Link } from 'react-router-dom';
+
 import Tilt from '../components/Tilt';
 import './GlassmorphismCard.scss';
+
+const cards = Array.from({ length: 9 }, (_, i) => i + 1);
 
 function GlassmorphismCard(): JSX.Element {
   return (
     <main className="GlassmorphismCard">
       <div className="container">
-        <Tilt className="card">
-          <div className="card-content">
-            <h2>01</h2>
-            <h3>Card</h3>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit
-              maiores neque at. At possimus saepe aliquid minima recusandae
-              facilis quas repellat, voluptates impedit culpa magnam dicta
-              commodi veniam hic accusantium!
-            </p>
-            <a href="/">Read more</a>
-          </div>
-        </Tilt>
-        <Tilt className="card">
-          <div className="card-content">
-            <h2>02</h2>
-            <h3>Card</h3>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit
-              maiores neque at. At possimus saepe aliquid minima recusandae
-              facilis quas repellat, voluptates impedit culpa magnam dicta
-              commodi veniam hic accusantium!
-            </p>
-            <a href="/">Read more</a>
-          </div>
-        </Tilt>
-        <Tilt className="card">
-          <div className="card-content">
-            <h2>03</h2>
-            <h3>Card</h3>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit
-              maiores neque at. At possimus saepe aliquid minima recusandae
-              facilis quas repellat, voluptates impedit culpa magnam dicta
-              commodi veniam hic accusantium!
-            </p>
-            <a href="/">Read more</a>
-          </div>
-        </Tilt>
+        {cards.map((value) => (
+          <Tilt className="card" key={value}>
+            <div className="card-content">
+              <h2>0{value}</h2>
+              <h3>Card 0{value}</h3>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Suscipit maiores neque at. At possimus saepe aliquid minima
+                recusandae facilis quas repellat, voluptates impedit culpa
+                magnam dicta commodi veniam hic accusantium!
+              </p>
+              <Link to="/">Read more</Link>
+            </div>
+          </Tilt>
+        ))}
       </div>
     </main>
   );
